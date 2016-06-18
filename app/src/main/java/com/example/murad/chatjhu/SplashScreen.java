65fcd1bb5a7e3d45cca7c683469b85ae89a9875b
free4murad <murad.kuka@gmail.com>
2016-06-19 01:43:02 +0530

    Start

--- app/src/main/java/com/example/murad/chatjhu/SplashScreen.java
+++ app/src/main/java/com/example/murad/chatjhu/SplashScreen.java
@@ -3,10 +3,12 @@ package com.example.murad.chatjhu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
     }
